51cedb6ab0a30fae77eb2fc18f4bb27476cc22db
Alex Darvin <amdarvin14@gmail.com>
2020-09-20 19:15:39 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  #attr_accessor and attr_reader make it so we dont need to define methods for every instance variable. the methods for title, page_count, genre, and author arent needed here if attributes readers and accessors are used.
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+    
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+  
+end
+
 
 
